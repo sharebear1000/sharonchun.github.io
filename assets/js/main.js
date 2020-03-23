e48fd4012a8409668a33ef1d4b61284f939bd443
sharebear1000 <61988255+sharebear1000@users.noreply.github.com>
2020-03-23 13:43:57 -0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$(function() {
+
+	// Vars.
+		var	$window = $(window),
+			$body = $('body'),
+			$wrapper = $('#wrapper');
+
+	// Breakpoints.
+		skel.breakpoints({
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 736px)',
+			xsmall:	'(max-width: 480px)'
+		});
+
+	// Poptrox.
+		$window.on('load', function() {
+
+			$('.thumbnails').poptrox({
+				overlayColor: '#ffffff',
+			});
+
+		});
+
+});
